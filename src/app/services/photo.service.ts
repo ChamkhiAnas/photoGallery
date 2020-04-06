4dd55cf054610bd5f226d520f5c1c077747a0296
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-06 12:03:48 +0100

    PhotosView1

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -14,7 +14,46 @@ export class PhotoService {
 
   constructor() { }
 
- private async savePicture(cameraPhoto: CameraPhoto) { }
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;  
+  }
+  
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+  private async getPhotoFile(cameraPhoto: CameraPhoto, 
+    fileName: string): Promise<Photo> {
+return {
+filepath: fileName,
+webviewPath: cameraPhoto.webPath
+};
+}
+
+ private async savePicture(cameraPhoto: CameraPhoto) { 
+     // Convert photo to base64 format, required by Filesystem API to save
+  const base64Data = await this.readAsBase64(cameraPhoto);
+
+  // Write the file to the data directory
+  const fileName = new Date().getTime() + '.jpeg';
+  await Filesystem.writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: FilesystemDirectory.Data
+  });
+
+  // Get platform-specific photo filepaths
+  return await this.getPhotoFile(cameraPhoto, fileName);
+
+ }
 
 
 public async addNewToGallery() {
@@ -42,21 +81,6 @@ public async addNewToGallery() {
 
 }
 
-private async savePicture(cameraPhoto: CameraPhoto) {
-  // Convert photo to base64 format, required by Filesystem API to save
-  const base64Data = await this.readAsBase64(cameraPhoto);
-
-  // Write the file to the data directory
-  const fileName = new Date().getTime() + '.jpeg';
-  await Filesystem.writeFile({
-    path: fileName,
-    data: base64Data,
-    directory: FilesystemDirectory.Data
-  });
-
-  // Get platform-specific photo filepaths
-  return await this.getPhotoFile(cameraPhoto, fileName);
-}
 
 
 }
