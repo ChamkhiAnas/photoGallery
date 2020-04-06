813fd3142501832001a7e486aa4b28bf502e2232
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-06 10:40:41 +0100

    camera

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource } from '@capacitor/core';
+const { Camera, Filesystem, Storage } = Plugins;
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,19 @@ import { Injectable } from '@angular/core';
 export class PhotoService {
 
   constructor() { }
+
+public async addNewToGallery() {
+  // Take a photo
+  console.log("hello world")
+  console.log("hello world")
+
+  
+
+  const capturedPhoto = await Camera.getPhoto({
+    resultType: CameraResultType.Uri, 
+    source: CameraSource.Camera, 
+    quality: 100 
+  });
+}
+
 }
