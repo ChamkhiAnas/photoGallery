4dbc4c52d4c8dd6ac40b44c0d7213bf480bcfa49
ChamkhiAnas <mahmoudbennouna@gmail.com>
2020-04-07 11:22:02 +0100

    save in localstorage

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,135 +1,104 @@
-import { Injectable } from '@angular/core';
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource } from '@capacitor/core';
-const { Camera, Filesystem, Storage } = Plugins;
 
+import { Injectable } from '@angular/core';
+import {
+  Plugins, CameraResultType, Capacitor, FilesystemDirectory,
+  CameraPhoto, CameraSource
+} from '@capacitor/core';
+
+const { Camera, Filesystem, Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
-
   public photos: Photo[] = [];
 
-  // other code
+  private PHOTO_STORAGE: string = "photos";
 
-    private PHOTO_STORAGE: string = "photos";
-    // Display the photo by reading into base64 format
+  constructor() {
+  }
 
- 
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
 
-  constructor() { }
-  
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos.map(p => {
+        const photoCopy = { ...p };
+        delete photoCopy.base64;
+        return photoCopy;
+      }))
+    });
+  }
+
   public async loadSaved() {
-    // Retrieve cached photo array data
     const photos = await Storage.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photos.value) || [];
-  
-    // more to come...
-  }
-  
 
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: FilesystemDirectory.Data
+      });
+
+      // Web platform only: Save the photo into the base64 field
+      photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Get platform-specific photo filepaths
+    return await this.getPhotoFile(cameraPhoto, fileName);
+
+  }
   private async readAsBase64(cameraPhoto: CameraPhoto) {
     // Fetch the photo, read as a blob, then convert to base64 format
     const response = await fetch(cameraPhoto.webPath!);
     const blob = await response.blob();
-  
-    return await this.convertBlobToBase64(blob) as string;  
+
+    return await this.convertBlobToBase64(blob) as string;
   }
-  
+
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader;
+    const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
-        resolve(reader.result);
+      resolve(reader.result);
     };
     reader.readAsDataURL(blob);
   });
-  private async getPhotoFile(cameraPhoto: CameraPhoto, 
-    fileName: string): Promise<Photo> {
-return {
-filepath: fileName,
-webviewPath: cameraPhoto.webPath
-};
-}
 
- private async savePicture(cameraPhoto: CameraPhoto) { 
-     // Convert photo to base64 format, required by Filesystem API to save
-  const base64Data = await this.readAsBase64(cameraPhoto);
-
-  // Write the file to the data directory
-  const fileName = new Date().getTime() + '.jpeg';
-  await Filesystem.writeFile({
-    path: fileName,
-    data: base64Data,
-    directory: FilesystemDirectory.Data
-  });
-
-  // Get platform-specific photo filepaths
-  return await this.getPhotoFile(cameraPhoto, fileName);
-
- }
-
-
-public async addNewToGallery() {
-  // Take a photo
-  console.log("hello world")
-  console.log("hello world")
-
-  
-
-  const capturedPhoto = await Camera.getPhoto({
-    resultType: CameraResultType.Uri, 
-    source: CameraSource.Camera, 
-    quality: 100 
-  });
-
-    // Save the picture and add it to photo collection
-    const savedImageFile:any = await this.savePicture(capturedPhoto);
-    this.photos.unshift(savedImageFile);
-  
-
-  // this.photos.unshift({
-  //   filepath: "sn...",
-  //   webviewPath: capturedPhoto.webPath
-  // });
-
-  Storage.set({
-    key: this.PHOTO_STORAGE,
-    value: JSON.stringify(this.photos.map(p => {
-            // Don't save the base64 representation of the photo data, 
-            // since it's already saved on the Filesystem
-            const photoCopy = { ...p };
-            delete photoCopy.base64;
-  
-            return photoCopy;
-            }))
-  });
-  
-  for (let photo of this.photos) {
-    // Read each saved photo's data from the Filesystem
-    const readFile = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: FilesystemDirectory.Data
-    });
-  
-    // Web platform only: Save the photo into the base64 field
-    photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+  private async getPhotoFile(cameraPhoto: CameraPhoto, fileName: string): Promise<Photo> {
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
   }
-  
 
 }
-
-
-
-
-}
-
-
-
-
 
 interface Photo {
   filepath: string;
   webviewPath: string;
   base64?: string;
 }
+
